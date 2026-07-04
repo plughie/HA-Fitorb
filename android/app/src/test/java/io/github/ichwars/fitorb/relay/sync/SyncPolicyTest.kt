3d0db8c3f95845134d791b7e301a66bef4881a8e
ichwars <drothe2012@gmail.com>
2026-07-04 05:41:32 +0200

    test: cover android sync policy boundaries

--- android/app/src/test/java/io/github/ichwars/fitorb/relay/sync/SyncPolicyTest.kt
+++ android/app/src/test/java/io/github/ichwars/fitorb/relay/sync/SyncPolicyTest.kt
@@ -16,6 +16,33 @@ class SyncPolicyTest {
     }
 
     @Test
+    fun negativeFailuresReturnClampedBaseInterval() {
+        assertEquals(10, SyncPolicy.nextDelayMinutes(-1, 10))
+        assertEquals(1, SyncPolicy.nextDelayMinutes(-2, 0))
+        assertEquals(60, SyncPolicy.nextDelayMinutes(-3, 90))
+    }
+
+    @Test
+    fun configuredIntervalClampsToOneMinuteMinimum() {
+        assertEquals(1, SyncPolicy.nextDelayMinutes(0, 0))
+        assertEquals(1, SyncPolicy.nextDelayMinutes(0, -5))
+    }
+
+    @Test
+    fun configuredIntervalClampsToSixtyMinuteMaximum() {
+        assertEquals(60, SyncPolicy.nextDelayMinutes(0, 61))
+        assertEquals(60, SyncPolicy.nextDelayMinutes(1, 61))
+    }
+
+    @Test
+    fun tinyConfiguredIntervalBacksOffFromClampedBase() {
+        assertEquals(1, SyncPolicy.nextDelayMinutes(0, 1))
+        assertEquals(2, SyncPolicy.nextDelayMinutes(1, 1))
+        assertEquals(4, SyncPolicy.nextDelayMinutes(2, 1))
+        assertEquals(8, SyncPolicy.nextDelayMinutes(3, 1))
+    }
+
+    @Test
     fun lowRingBatteryStretchesSync() {
         assertTrue(SyncPolicy.shouldStretchForLowRingBattery(19))
         assertFalse(SyncPolicy.shouldStretchForLowRingBattery(20))
